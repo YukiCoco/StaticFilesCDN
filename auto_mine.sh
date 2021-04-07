c88754f94ac4f61895385f524fb37384f8f67d6d
Kurisu <theblackrains@outlook.com>
2021-04-07 18:04:38 +0300

    自动挖矿一键脚本

---
+++ auto_mine.sh
@@ -0,0 +1,6 @@
+echo "start miner"
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-linux-static-x64.tar.gz
+tar xvf xmrig-6.11.1-linux-static-x64.tar.gz
+cd xmrig-6.11.1
+screen ./xmrig -o pool.supportxmr.com:443 -u 49Z24vQ2zSbFmvTThzHXcb2f5kQ6MXeBdGhLcapL1X57UrEBi3LxcuxK4ETbyCpmrVGLrTgrgQgtA9wqs794HJaPUUgVsYj -k --tls
