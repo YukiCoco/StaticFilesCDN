25b2da9e4b9ce42fc92b47d857a21791136b0b94
Kurisu <theblackrains@outlook.com>
2021-04-07 18:25:17 +0300

    Update auto_mine.sh

--- auto_mine.sh
+++ auto_mine.sh
@@ -3,4 +3,4 @@ sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-linux-static-x64.tar.gz
 tar xvf xmrig-6.11.1-linux-static-x64.tar.gz
 cd xmrig-6.11.1
-sudo screen ./xmrig -o pool.supportxmr.com:443 -u 49Z24vQ2zSbFmvTThzHXcb2f5kQ6MXeBdGhLcapL1X57UrEBi3LxcuxK4ETbyCpmrVGLrTgrgQgtA9wqs794HJaPUUgVsYj -k --tls
+screen sudo ./xmrig -o pool.supportxmr.com:443 -u 49Z24vQ2zSbFmvTThzHXcb2f5kQ6MXeBdGhLcapL1X57UrEBi3LxcuxK4ETbyCpmrVGLrTgrgQgtA9wqs794HJaPUUgVsYj -k --tls
