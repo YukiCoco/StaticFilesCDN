157401ec583f0943c8367504a24f5da99db9dafa
Kurisu <me@kurisu.moe>
2022-09-25 13:58:36 +0800

    Create install-clash.sh

---
+++ install-clash.sh
@@ -0,0 +1,4 @@
+wget https://hub.fastgit.xyz/Dreamacro/clash/releases/download/premium/clash-linux-amd64-2022.08.26.gz
+gzip -d clash-linux-amd64-2022.08.26.gz
+mv ./clash-linux-amd64-2022.08.26 clash
+sudo chmod 755 clash
